779c984aee97f08a9291fb104969c19233180214
Ashutosh Das <ashutoshdas75@gmail.com>
2026-03-29 08:05:37 +0000

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AD_Medical_Chatbot",
+    version="0.1.0",
+    author="Ashutosh Das",
+    author_email="ashutoshdas75@gmail.com",
+    description="A medical chatbot for general health query",
+    packages=find_packages(),
+    install_requires=[]
+)
