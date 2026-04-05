325e51f7cbbc82260660c9821c87b76e00a6a2a8
Ashutosh Das <ashutoshdas75@gmail.com>
2026-04-05 18:54:47 +0000

    Dyna Port

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, log_level="debug")
